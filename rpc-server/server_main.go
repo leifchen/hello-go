2e749c3b672a036835d65c7df395c6b7b5e3526f
LeifChen <leifchen90@gmail.com>
2021-01-19 11:39:29 +0800

    feat: add RPC based on HTTP

--- rpc-server/server_main.go
+++ rpc-server/server_main.go
@@ -4,14 +4,16 @@ import (
 	"hello-go/rpc-server/server"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
 )
 
 func main() {
 	rpc.RegisterName("MathService", new(server.MathService))
+	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
-	rpc.Accept(l)
+	http.Serve(l, nil)
 }
