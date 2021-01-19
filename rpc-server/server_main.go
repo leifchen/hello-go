a63ef00b7d7d71901197fa1447eafd2db657c1a7
LeifChen <leifchen90@gmail.com>
2021-01-19 11:43:06 +0800

    feat: add JSON RPC based on HTTP

--- rpc-server/server_main.go
+++ rpc-server/server_main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"hello-go/rpc-server/server"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
 
 func main() {
 	rpc.RegisterName("MathService", new(server.MathService))
+	//注册一个path，用于提供基于http的json rpc服务
+	http.HandleFunc(rpc.DefaultRPCPath, func(rw http.ResponseWriter, r *http.Request) {
+		conn, _, err := rw.(http.Hijacker).Hijack()
+		if err != nil {
+			log.Print("rpc hijacking ", r.RemoteAddr, ": ", err.Error())
+			return
+		}
+		var connected = "200 Connected to JSON RPC"
+		io.WriteString(conn, "HTTP/1.0 "+connected+"\n\n")
+		jsonrpc.ServeConn(conn)
+	})
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Println("jsonrpc.Serve: accept:", err.Error())
-			return
-		}
-		go jsonrpc.ServeConn(conn)
-	}
+	http.Serve(l, nil)
 }
